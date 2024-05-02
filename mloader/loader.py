3e5ebf37464deaf29b5b6a5020cb1f8e1606d840
Rokib Hasan Sagar <10854164+rokibhasansagar@users.noreply.github.com>
2024-05-03 00:41:28 +0600

    loader.py: Update Internal API App Version, Again
    Signed-off-by: rokibhasansagar <10854164+rokibhasansagar@users.noreply.github.com>

--- mloader/loader.py
+++ mloader/loader.py
@@ -24,7 +24,7 @@ log = logging.getLogger()
 MangaList = Dict[int, Set[int]]  # Title ID: Set[Chapter ID]
 
 AUTH_PARAMS = {
-    "app_ver": "124",
+    "app_ver": "165",
     "os": "android",
     "os_ver": "25",
     "secret": "d57128c0e0aef2df30af248b68827e9c",
